122aea0e005b0ad256ed88cd30f5fe9758462547
SkyMinus <minus_rav@qq.com>
2025-01-25 15:24:03 +0800

    fuxi: Set permissions for touch sensors
    Change-Id: I777ed71ecc5dc502182567bc7e1166605251af93
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- device.mk
+++ device.mk
@@ -175,6 +175,7 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.xiaomi_sm8550.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.xiaomi_sm8550.rc \
+    $(LOCAL_PATH)/init/init.fuxi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.fuxi.rc \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 # IPA
